382ec16239e87cfdcdfa4783a842df473633173e
PascalRohart <pascalrohart3@orange.fr>
2022-03-14 21:40:14 +0100

    ajout des différentes pages de l'applications

--- app/src/main/java/com/example/calculmental/MainActivity.java
+++ app/src/main/java/com/example/calculmental/MainActivity.java
@@ -2,13 +2,33 @@
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+ public class MainActivity extends AppCompatActivity {
+
+    private Button play;
+    private Button scores;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.play = findViewById(R.id.btnjouer);
+        this.scores = findViewById(R.id.btnscores);
+
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent compute_activity = new Intent(getApplicationContext(), CalculActivity.class);
+                startActivity(compute_activity);
+                finish();
+            }
+
+
+        });
     }
 }
---
+++ app/src/main/java/com/example/calculmental/ScoreActivity.java
@@ -0,0 +1,4 @@
+package com.example.calculmental;
+
+public class ScoreActivity {
+}
